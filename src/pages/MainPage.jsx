a8e04ed82c2973ffdc387bf83fc1119ab243c2fd
KatheRojasO <79369847+KatheRojasO@users.noreply.github.com>
2023-01-17 13:38:12 +0100

    Set up categories for mobile

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,10 +3,10 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import data from "../jsonFiles/mainpage.json";
-import img1 from "../assets/img/background-image-1(1).png";
-import img2 from "../assets/img/background-image-2(1).png";
-import img3 from "../assets/img/background-image-3(1).png";
-import img4 from "../assets/img/background-image-4(1).png";
+import img1 from "../assets/img/background-image-1.png";
+import img2 from "../assets/img/background-image-2.png";
+import img3 from "../assets/img/background-image-3.png";
+import img4 from "../assets/img/background-image-4.png";
 
 export default function MainPage() {
   const items = data.map((item) => (
@@ -23,10 +23,10 @@ export default function MainPage() {
       <Header />
       <Hero />
       <div className="container">
-        <img src={img1} className="item1" alt="" />
-        <img src={img2} className="item2" alt="" />
-        <img src={img3} className="item3" alt="" />
-        <img src={img4} className="item4" alt="" />
+        <img src={img1} className="bg-1" alt="" />
+        <img src={img2} className="bg-2" alt="" />
+        <img src={img3} className="bg-3" alt="" />
+        <img src={img4} className="bg-4" alt="" />
         <p className="page-description">
           A refreshingly inviting, communal and upbeat restaurant featuring
           seasonally and creative vegan cuisine that resembles what other might
