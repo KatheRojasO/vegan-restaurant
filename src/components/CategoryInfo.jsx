0378dd2742288e511420aeca70abd85de6ba363e
KatheRojasO <79369847+KatheRojasO@users.noreply.github.com>
2023-02-09 11:34:59 +0100

    Delete background images from all the pages

--- src/components/CategoryInfo.jsx
+++ src/components/CategoryInfo.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import img1 from "../assets/img/background-image-1.png";
-import img2 from "../assets/img/background-image-2.png";
-import img3 from "../assets/img/background-image-3.png";
-import img4 from "../assets/img/background-image-4.png";
 
 export default function CategoryInfo({ categoryInfo, categoryName }) {
   const { title, description, image, alt, options } = categoryInfo;
@@ -31,11 +27,7 @@ export default function CategoryInfo({ categoryInfo, categoryName }) {
         <img src={image} alt={alt}></img>
       </div>
       <div className="container">
-        <img src={img1} className="bg-1" alt="" />
         <p className="category-description">{description}</p>
-        <img src={img2} className="bg-2" alt="" />
-        <img src={img3} className="bg-3" alt="" />
-        <img src={img4} className="bg-4" alt="" />
         {menuItems}
       </div>
     </div>
